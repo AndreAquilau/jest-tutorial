c9e38db33e6e84f045b0dc3b5181af9be922a5e0
AndreAquilau <andre.aquilau@outlook.com>
2021-02-04 14:48:48 -0400

    test: created test discount

--- src/classes/discount-tenDiscount.spec.ts
+++ src/classes/discount-tenDiscount.spec.ts
@@ -1,3 +1,11 @@
+import { TenPercentDiscount } from './discount';
+
+const createSut = () => new TenPercentDiscount();
+
 describe('TenPercentDiscount', () => {
   afterEach(() => jest.clearAllMocks());
+
+  test('should return discount with value 0.1', () => {
+    const sut = createSut();
+  });
 });
